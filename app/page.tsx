18a3ea70cf51a5e842c89bec23f5d5d5ee28a4b5
Miguel Pacheco <angelpachecogranillo@gmail.com>
2024-03-01 23:59:46 -0600

    add input and carousel into page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
+import HeroCarousel from "@/components/HeroCarousel";
+import Searchbar from "@/components/Searchbar";
 import Image from "next/image";
 
 export default function Home() {
     return (
         <>
-            <section className='px-6 md:px-20 py-24 border-2 border-red-500'>
-                <div className="flex max-xl:flex-col">
+            <section className='px-6 md:px-20 py-24'>
+                <div className="flex max-xl:flex-col gap-16">
                     <div className="flex flex-col justify-center">
                         <p className="small-text">
                             Smart Shopping Starts Here:
@@ -25,10 +27,10 @@ export default function Home() {
                             Powerful, self-serve product and growth analytics to help you convert, engage, and retain more.
                         </p>
 
-                        Searchbar
+                        <Searchbar/>
                     </div>
 
-                    HeroCrousel
+                    <HeroCarousel/>
                 </div>
             </section>
 
