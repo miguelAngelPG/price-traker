cc7495500005f497fb47a5d4642e933d4d3dd15f
Miguel Pacheco <angelpachecogranillo@gmail.com>
2024-03-03 05:06:16 -0600

    modal to send mail

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,3 +1,4 @@
+import Modal from '@/components/Modal'
 import { PriceInfoCard } from '@/components/PriceInfoCard'
 import { ProductCard } from '@/components/ProductCard'
 import { getProductById, getSimilarProducts } from '@/lib/actions'
@@ -141,7 +142,8 @@ const ProductPage = async ({ params: { id } }: Props) => {
                             />
                         </div>
                     </div>
-                    Modal
+                    
+                    <Modal productId={ id }/>
                 </div> 
             </div>
             
--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -5,6 +5,8 @@ import Product from "../models/product.model"
 import { connectToDB } from "../mongoose"
 import { scrapeAmazonProduct } from "../scraper"
 import { getAveragePrice, getHighestPrice, getLowestPrice } from "../utils"
+import { User } from "@/types"
+import { generateEmailBody, sendEmail } from "../nodemailer"
 
 export async function scrapeAndStoreProduct(productUrl: string) {
     if(!productUrl) return;
@@ -92,3 +94,26 @@ export async function getSimilarProducts(productId: string) {
       console.log(error);
     }
 } 
+
+export async function addUserEmailToProduct(productId: string, userEmail: string) {
+    try {
+      const product = await Product.findById(productId);
+  
+      if(!product) return;
+  
+      const userExists = product.users.some((user: User) => user.email === userEmail);
+        
+      console.log(userExists)
+      if(!userExists) {
+        product.users.push({ email: userEmail });
+  
+        await product.save();
+  
+        const emailContent = await generateEmailBody(product, "WELCOME");
+  
+        await sendEmail(emailContent, [userEmail]);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+}
